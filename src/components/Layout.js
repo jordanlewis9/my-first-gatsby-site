a754d12c7af0dab74978ba76ed65ef49da99fbb9
Jordan Lewis <jordan@forefrontweb.com>
2022-02-14 12:37:30 -0500

    finished part 4 of gatsby tutorial

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,6 +21,7 @@ query {
           <ul className={navLinks}>
             <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
             <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+            <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
           </ul>
         </nav>
         <main>
