852c479c7d12572986caa4b6f4027d4399081384
Jordan Lewis <jordan@forefrontweb.com>
2022-02-14 09:42:40 -0500

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <Layout pageTitle="About Me">
+                <p>Hello there! My name is Jordan, and I am a developer based in the great state of Ohio!</p>
+            </Layout>
+        </main>
+    )
+}
+
+export default AboutPage;
