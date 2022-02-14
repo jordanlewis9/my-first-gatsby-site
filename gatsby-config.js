2a6f8ef300e4eb68a7c685aaac294277b3e2a58f
Jordan Lewis <jordan@forefrontweb.com>
2022-02-14 10:40:34 -0500

    Midway through part 4 tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-      title: `My First Gatsby Site`,
+      title: `My first gatsby site`,
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
